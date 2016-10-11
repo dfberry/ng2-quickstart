158850a5e89bb287702705d5bcafa10b79c40220
Dina Berry <dinaberry@outlook.com>
2016-10-11 14:48:35 -0700

    added html file with button, event, and styles

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    templateUrl: 'app/app.component.html'
 })
-export class AppComponent { }
+export class AppComponent { 
+    title = 'Blog Log';
+    name = 'test';
+    wardsColor = 'blue';
+
+    //[] property binding
+    //()event binding
+
+    changeColor(){
+        this.wardsColor = this.wardsColor === 'blue' ? 'red' : 'blue';
+    }
+}
