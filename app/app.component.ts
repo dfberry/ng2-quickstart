46a9ac605f8d42296bb20433c4f7c484ab023f81
Dina Berry <dinaberry@outlook.com>
2016-10-12 13:22:58 -0700

    using dependency from parent

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { CustomersComponent } from './customer/customers.component';
+import { CustomerService } from './customer/customer.service';
 @Component({
     moduleId: module.id, //system js variable name for relative path
     selector: 'my-app',
-    templateUrl: 'app.component.html'
+    templateUrl: 'app.component.html',
+    providers: [CustomerService] // app level providers
 })
 export class AppComponent { 
     title = 'Blog Log';
--- app/customer/customer.component.ts
+++ app/customer/customer.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { CustomerService } from './customer.service'; // using CustomerService from customers
 
 @Component({
     moduleId: module.id,
