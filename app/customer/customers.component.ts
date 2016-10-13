37c5fefa6aaabf9db873a00ee2198460922cd46f
dfberry <dfberry@admins-MacBook-Air.local>
2016-10-13 09:03:58 -0700

    comment out line 20 for npm start then uncomment it

--- app/customer/customers.component.ts
+++ app/customer/customers.component.ts
@@ -11,13 +11,14 @@ import { CustomerService } from './customer.service';
 })
 export class CustomersComponent implements OnInit {
     customers: Observable<any>[];
+    errorMsg: string;
 
     constructor(private _customerService: CustomerService){}
 
     // not called automatically by testing
     // but is called by angular
     ngOnInit(){
-        this.customers =this._customerService.getCustomers();
+        this.customers = this._customerService.getCustomers();
     }
 
 }
